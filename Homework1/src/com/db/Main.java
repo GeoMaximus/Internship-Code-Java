5561fbb64d4dfb88c268405649939233eecaa6ae
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-03-16 18:02:17 +0200

    Final homework version

--- Homework1/src/com/db/Main.java
+++ Homework1/src/com/db/Main.java
@@ -15,7 +15,6 @@ Create a Vehicle class [that implements Comparable interface].
 
 package com.db;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,14 +22,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        //create buses
         Bus b1 = new Bus();
         Bus b2 = new Bus("red", true, 20, 14.5f);
         Bus b3 = new Bus("yellow", false, 30, 3.2f);
 
+
+        //create taxis
         Taxi t1 = new Taxi();
         Taxi t2 = new Taxi("black", true, 3, 35.5f);
         Taxi t3 = new Taxi("yellow", false, 2, 20.4f);
 
+
+        //create the list and populate it
         List<Vehicle> vehicleList = new ArrayList<>();
         vehicleList.add(b1);
         vehicleList.add(b2);
@@ -39,47 +44,59 @@ public class Main {
         vehicleList.add(t2);
         vehicleList.add(t3);
 
+
+        //initalize the busStation
         BusStation busStation = new BusStation(vehicleList);
 
+
+        //testing getters and setters
+        System.out.println("Bus 1 info: ");
         System.out.println(b1.getColour());
         System.out.println(b1.isFunctional());
 
         b1.setColour("another one");
         b1.setFunctional(true);
 
+        System.out.println("Bus 1 info after using setters: ");
         System.out.println(b1.getColour());
         System.out.println(b1.isFunctional());
 
         System.out.println("----------------------");
 
-        System.out.println("For bus B2:");
-        System.out.println(b2.charge());
-        System.out.println(b2.profit());
+        //testing the abstract methods
+        System.out.println("Abstract methods for bus B2:");
+        System.out.println("Bus2 charge is: " + b2.charge());
+        System.out.println("Bus2 profit is: " + b2.profit());
         b2.display();
 
-        System.out.println("For bus B3:");
-        System.out.println(b3.charge());
-        System.out.println(b3.profit());
-        b3.display();
+        System.out.println("Abstract methods for bus T3:");
+        System.out.println("Taxi3 charge is: " + t3.charge());
+        System.out.println("Taxi3 charge is: " + t3.profit());
+        t3.display();
 
+
+        System.out.println("Testing compareTo method");
+        //testing compareTo
         if(b2.compareTo(b3) == 1) {
-            System.out.println("First bus has more profit");
+            System.out.println("First bus has more profit" + "Vehicle1: " + b2.profit() + "and Vehicle2: " + b3.profit());
         }
         else if( b2.compareTo(b3) == -1){
-            System.out.println("Second bus has more profit");
+            System.out.println("Second bus has more profit" + "Vehicle1: " + b2.profit() + "and Vehicle2: " + b3.profit());
         }
         else
-            System.out.println("They have equal profit");
+            System.out.println("They have equal profit" + "Bus1: " + "Vehicle1: " + b2.profit() + "and Vehicle2: " + b3.profit());
 
-        System.out.println("----------------------");
+        System.out.println("----------Displaying all vehicles------------");
+
+        //testing the methods for busStation
 
         busStation.showAllVehicles();
 
-        System.out.println("----------------------");
+        System.out.println("------------Displaying total profit----------");
 
         System.out.println(busStation.showTotalProfit());
 
-        System.out.println("----------------------");
+        System.out.println("----------Displaying the sorted list based on profit------------");
 
         for(Vehicle v : busStation.sortVehicles()){
             v.display();
