dd3bad256bc79ced4d071ba955ccec5bba00539c
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-03-16 11:24:29 +0200

    Arrays, Classes

--- JavaCourse02/src/com/db/Main.java
+++ JavaCourse02/src/com/db/Main.java
@@ -1,25 +1,68 @@
 package com.db;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello world");
+//	System.out.println("Hello world");
     int Sum = 0;
     for (int i = 0;i<10;i++){
         Sum+=i;
     }
 
-    System.out.println(Sum);
+//    System.out.println(Sum);
 
     Car car = new Car();
 
     Car car1 = new Car(1234589, "Toyota", 220);
-    System.out.println(car1.getBrand());
-    System.out.println((car1.getSerialNo()));
-    System.out.println(car1.getMaxSpeed());
+//    System.out.println(car1.getBrand());
+//    System.out.println((car1.getSerialNo()));
+//    System.out.println(car1.getMaxSpeed());
+//
+//    System.out.println(car1);
 
-    System.out.println(car1);
+
+        int[] array0 = new int[5];
+        int[] array = new int[]{1,2,3,4,5};
+
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+
+        System.out.println(array[0]);
+
+        int sum = 0;
+        for(int i =0;i<array.length;i++){
+            sum += array[i];
+        }
+        System.out.println(sum);
+
+        int matrix[][] = new int[3][4];
+
+        int matrix1[][] = new int[][]{{1,2,3}, {4,5,6}, {7,8,9}, {10,11,12}};
+        System.out.println(matrix);
+
+        for (int[] ints : matrix1) {
+            for(int elem : ints){
+                System.out.print(elem+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("-------------------------");
+        
+        for (int i = 0; i < matrix1.length; i++) {
+            for(int y = 0; y< 3;y++){
+                System.out.print(matrix1[i][y] + " ");
+        }
+            System.out.println();
+
+            }
+
+        }
+
 
 
     }
-}
---
+++ JavaCourse02/src/com/db/MainClass.java
@@ -0,0 +1,29 @@
+package com.db;
+
+public class MainClass {
+    public static void main(String[] args){
+        Human h1 = new Human();
+        Human h2 = new Human();
+        Human h3 = h1;
+
+        h1.firstName = "John";
+        h1.lastName = "Doe";
+        h1.age = 12;
+        h1.gender = 'M';
+
+        
+
+        System.out.println(h1.firstName);
+        System.out.println(h3.firstName);
+    }
+
+
+}
+
+class Human {
+    String firstName;
+    String lastName;
+    int age;
+    char gender;
+
+}
