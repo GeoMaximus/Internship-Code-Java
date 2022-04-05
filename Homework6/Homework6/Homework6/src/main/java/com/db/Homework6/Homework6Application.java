b8ba35cd96a2e2e149b22d9e5d330ce4241b094f
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-04-05 16:51:16 +0300

    Homework6 1st attempt

---
+++ Homework6/Homework6/Homework6/src/main/java/com/db/Homework6/Homework6Application.java
@@ -0,0 +1,13 @@
+package com.db.Homework6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework6Application.class, args);
+	}
+
+}
---
+++ Homework6/Homework6/Homework6/src/test/java/com/db/Homework6/Homework6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.Homework6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
