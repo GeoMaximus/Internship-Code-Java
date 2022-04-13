57c21051b99ff3d723676f083049d7f9a180ffa3
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-04-13 20:35:05 +0300

    project created & application-local added

---
+++ Homework08/demo/demo/src/main/java/com/db/Homework08Application.java
@@ -0,0 +1,13 @@
+package com.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework08Application.class, args);
+	}
+
+}
